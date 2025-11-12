5622fd42cb206a2fb816ce66870b00cf18aef459
Rohan <rohan.john@firmusoft.co.in>
2025-11-12 17:21:24 +0530

    eslint config updated

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,28 @@
 const config = require('@modusoperandi/eslint-config');
+const baseConfig = config.getFlatConfig({
+  strict: false,
+  strict: false,
+  header: config.header.mit
+});
+
+// Find and modify the config object that has parserOptions
+const modifiedConfig = baseConfig.map(cfg => {
+  if (cfg.languageOptions?.parserOptions?.project) {
+    return {
+      ...cfg,
+      languageOptions: {
+        ...cfg.languageOptions,
+        parserOptions: {
+          ...cfg.languageOptions.parserOptions,
+          project: undefined, // Remove project since projectService is enabled
+        },
+      },
+    };
+  }
+  return cfg;
+});
 module.exports = [
-  ...config.getFlatConfig({
-    strict: false,
-    header: config.header.mit
-  }),
+  ...modifiedConfig,
   {
     rules: {
       //Include any rule overrides here!
